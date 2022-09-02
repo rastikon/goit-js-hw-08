1866c9d37fe6015f1d01ee2bd131c7bf4235c170
Rostyslav Kononenko <rastikon@gmail.com>
2022-09-02 20:26:44 +0200

    Homework done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+populateInput();
+
+form = document.querySelector('.feedback-form');
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInputForm, 500));
+
+//Відправка форми
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+//Збирає дані введені в полях input
+function onInputForm(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+//Фукнція вставляє дані із localStorage
+function populateInput() {
+  input = document.querySelector('input');
+  textarea = document.querySelector('textarea');
+
+  if (localStorage.getItem(STORAGE_KEY)) {
+    input.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+    textarea.value = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
+  }
+}
