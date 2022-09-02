814f1da91e60fb14e3c51411b1f0db036cc5ec1d
Rostyslav Kononenko <rastikon@gmail.com>
2022-09-02 09:39:46 +0200

    homework 8.2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,47 @@
-{
-  /* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-<script src="https://player.vimeo.com/api/player.js"></script>
-<script>
-    const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-</script> */
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(pause, 1000));
+
+function pause(timeupdate) {
+  let timeStoped = timeupdate.seconds;
+
+  localStorage.setItem('videoplayer-current-time', timeStoped);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+
+  if (currentTime) {
+    player
+      .setCurrentTime(currentTime)
+      .then(function (seconds) {
+        let round = Math.floor(seconds);
+        console.log('Відео починається з ' + round + '-ї секунди'); // seconds = the actual time that the player seeked to
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
 }
